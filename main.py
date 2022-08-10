2b3e13b9b97907587c1ef3094e9e792df878e9b5
dk.andersp@hotmail.com <iexDK98w>
2022-08-10 19:42:42 +0200

    new model

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 # Command prompt
 from model.inventory import Inventory
+from model.item import Item
 
 fridge = Inventory("Navn", "Bekrivelse")
 
+inv = Item("hej", "med", "dig", "hej", "med", "dig")
+
 print(fridge)
+
+print(inv.item_name)
---
+++ model/item.py
@@ -0,0 +1,9 @@
+class Item(object):
+    def __init__(self, item_name, item_description, item_date_added, item_amount, item_best_before, item_expiration):
+        self.item_name = item_name
+        self.item_descrription = item_description
+        self.item_date_added = item_date_added
+        self.item_amount = item_amount
+        self.item_best_before = item_best_before
+        self.item_expiration = item_expiration
+
